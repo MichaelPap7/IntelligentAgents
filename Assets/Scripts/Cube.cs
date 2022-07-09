9dab9b45747c3124ed32b5a2f5a29f030f17c080
IsidorosT <is.tsalapatis@gmail.com>
2022-07-10 00:35:34 +0300

    Some Structs

--- Assets/Scripts/Cube.cs
+++ Assets/Scripts/Cube.cs
@@ -1,8 +1,12 @@
 public class Cube
-    {
-        public int x { get; set; }
-        public int y { get; set; }
-        public PlaceContent value { get; set; }
+{
+    public int x { get; set; }
+    public int y { get; set; }
+    public PlaceContent value { get; set; }
 
-        public int Quantity { get; set; }
-    }
+    public int Quantity { get; set; }
+
+    public int coord_x { get; set; }
+    public int coord_y { get; set; }
+    public int coord_z { get; set; }
+}
